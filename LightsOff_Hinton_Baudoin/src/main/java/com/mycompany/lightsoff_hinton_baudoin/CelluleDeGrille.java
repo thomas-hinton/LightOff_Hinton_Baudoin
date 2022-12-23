7dee70c530b78672810f216483ab1775907f3d78
thomas hinton <thomas.hinton@epfedu.fr>
2022-12-24 00:05:29 +0100

    Création de la classe CelluleDeGrille: (Classe permettant de crére des objets de type CelluleDeGrille. Cette classe permet donc de gérer case par case si celle-ci est allumée ou non) -Définition de la classe et de ses attributs -Ajout d'un constructeur -Ajout de la methode lireCellule()  (permettant de savoir si son attribut lumiere_allumee est true or false : allumée ou eteinte) -Ajout de la méthode switchlight() (permettant de changer l'état d'une case allumée ou éteinte) -Ajout de la méthode toString()

---
+++ LightsOff_Hinton_Baudoin/src/main/java/com/mycompany/lightsoff_hinton_baudoin/CelluleDeGrille.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lightsoff_hinton_baudoin;
+
+/**
+ *
+ * @Créated by Mr Baudoin et Mr Hinton
+ */
+//Définition de la classe "CelluleDeGrille" 
+public class CelluleDeGrille {
+    private boolean lumiere_allumee ;
+    
+    /**
+     *Constructeur d'objet de type "CelluleDeGrille"
+     */
+    public CelluleDeGrille() {
+        lumiere_allumee = false;
+    }
+    
+    /**
+    * Méthode permettant de lire une cellule de grille et renvoyer si la lumière y est allumée ou pas
+    * @return lumiere_allumee (type : boolean) renvoie si la case possede sa lumière allumée (true/false)
+    */        
+    boolean lireCellule(){
+        return lumiere_allumee ;
+    }
+    
+    /**
+     * Méthode permettant de allummer / éteindre une cellule
+     */
+    public void switchlight(){
+        if (lumiere_allumee == true) {
+           lumiere_allumee = false ; 
+        }
+        else{
+            lumiere_allumee = true ;
+        }
+    }
+    
+    /**
+     * Méthode "toString()" qui permet de renvoyer l'état de la cellule : éteinte / allumée
+     * @return true/false (type : str)
+     */
+    @Override
+    public String toString() {      
+        String chaine_a_retourner ;
+        if (lumiere_allumee == true){            //Teste si la lumière est allumée ou éteinte
+            chaine_a_retourner = "true" ;
+        }
+        else{
+            chaine_a_retourner = "false" ;
+        }
+        return chaine_a_retourner ;
+    }    
+}
