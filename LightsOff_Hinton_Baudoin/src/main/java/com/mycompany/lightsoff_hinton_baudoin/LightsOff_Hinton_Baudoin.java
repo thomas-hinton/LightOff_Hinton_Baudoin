338fcb2a5702229ed30c3dfbf1cb86b1e91ba9b2
thomas hinton <thomas.hinton@epfedu.fr>
2022-12-23 23:09:50 +0100

    Création du jeu LightOff : -Création du répertoire -Rflexion avec MBaudoin sur la réalisation de ce projet

---
+++ LightsOff_Hinton_Baudoin/src/main/java/com/mycompany/lightsoff_hinton_baudoin/LightsOff_Hinton_Baudoin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lightsoff_hinton_baudoin;
+
+/**
+ *
+ * @Créated by Mr Baudoin et Mr Hinton
+ */
+public class LightsOff_Hinton_Baudoin {
+    public static void main(String[] args) {
+    }
+}
